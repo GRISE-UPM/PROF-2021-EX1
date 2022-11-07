705003ddde34f80a175743b26ad847f6e228b12e
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2022-11-07 11:25:23 +0100

    update pom.xml description

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,15 @@
 package es.upm.grise.profundizacion.control_1;
 
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
 public class LibraryTest {
 	
 	@Test
 	public void test() {
 		fail();
 	}
+
 
 }
