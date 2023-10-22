290027f381bc368ee193a5d2181f605ff0d8f587
kamiyuzu <alberto.rev.a@gmail.com>
2023-10-22 19:31:51 +0200

    Add tests

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,79 @@
 package es.upm.grise.profundizacion.control_1;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class LibraryTest {
-	
-	@Test
-	public void test() {
-		fail();
+
+	private Library library;
+
+	@BeforeEach
+	public void init() {
+		this.library = new Library();
 	}
 
+	@Test()
+	void getEmptyLibraryException() {
+		assertThrows(EmptyLibraryException.class, () -> {
+			this.library.getBook("Same book");
+		});
+	}
+
+	@Test()
+	void addBookToLibrary() throws DuplicatedBookException {
+		Book book = new Book("Same book");
+		this.library.addBook(book);
+	}
+
+	@Test()
+	void addDuplicatedBookToLibrary() throws DuplicatedBookException {
+		Book book = new Book("Same book");
+		this.library.addBook(book);
+
+		assertThrows(DuplicatedBookException.class, () -> {
+			this.library.addBook(book);
+		});
+	}
+
+	@Test()
+	void removeBookFromEmptyLibrary() {
+		Book book = new Book("Same book");
+
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			this.library.removeBook(book);
+		});
+	}
+
+	@Test()
+	void removeBookFromLibrary() throws DuplicatedBookException {
+		Book book = new Book("Same book");
+		this.library.addBook(book);
+		this.library.removeBook(book);
+	}
+
+	@Test()
+	void removeNonExistingBookFromLibrary() throws DuplicatedBookException {
+		Book book = new Book("Same book");
+		this.library.addBook(book);
+		Book non_existing_book = new Book("Non Existing book");
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			this.library.removeBook(non_existing_book);
+		});
+	}
+
+	@Test()
+	void getBookFromEmptyLibrary() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		assertThrows(EmptyLibraryException.class, () -> {
+			this.library.getBook("Same book");
+		});
+	}
+
+	@Test()
+	void getBookFromLibrary() throws DuplicatedBookException, NonExistingBookException, EmptyLibraryException {
+		Book book = new Book("Same book");
+		this.library.addBook(book);
+		this.library.getBook("Same book");
+	}
 }
